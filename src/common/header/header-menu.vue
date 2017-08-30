b14b61fbfdb4846c7f07e8aebd79d719c1c3bbf6
agzou <470841118@qq.com>
2017-08-30 23:48:57 +0800

    Index初始布局 by AgZou 2017-8-30 23:48:38

--- src/common/header/header-menu.vue
+++ src/common/header/header-menu.vue
@@ -1,6 +1,5 @@
 <template>
   <b-navbar toggleable="md" style="background-color:rgba(0,0,0,0.15);" type="dark" :sticky="isSticky">
-
     <b-nav-toggle target="nav_collapse"></b-nav-toggle>
 
     <b-navbar-brand href="#"><img src="../../assets/team_logo.png" height="34px" width="130px"></b-navbar-brand>
@@ -21,22 +20,43 @@
       </li>
     </ul>
     <b-nav is-nav-bar class="ml-auto">
-      <b-button variant="default" style="margin-right:3px">
-        <icon name="plus" scale="1" size="sm"></icon>
-      </b-button>
+      <b-nav-item-dropdown right variant="defalut" size="sm" no-caret="true">
+        <template slot="button-content">
+          <icon name="plus" scale="1" size="sm"></icon>
+        </template>
+        <div  class="newButton">
+          <div style="border-bottom: 1px rgb(140,140,140) inset;background-color:#F5F5F5 ">
+          <h5 class="text-center" >新建</h5>
+          </div>
+          <span>新建看板</span>
+          <p>一个看板包括多个列表,每个列表包括多个任务卡片,团队通过看板共享任务,在线实时协作</p>
+          <span>新建项目</span>
+          <p>一个项目包括多个看板和多名项目成员,通过项目可以对看板进行分组管理,更好的实现项目协作</p>
+        </div>
+      </b-nav-item-dropdown>
       <b-button-group size="sm" style="margin-right:3px">
         <b-button variant="default" size="sm">
           <icon name="bars" scale="1"></icon>
         </b-button>
-        <b-dropdown right variant="default" text="看板导航" size="sm">
+        <b-dropdown right variant="default" text="看板导航" size="sm" >
+          <menu-board></menu-board>
         </b-dropdown>
       </b-button-group>
       <b-button-group>
-      <sapn></sapn><img src="../../assets/agzou.jpg" width="32px" height="32px"></span>
-      <b-dropdown right variant="default" text="User" size="sm">
-        <b-dropdown-item href="#" to="/profile">Profile</b-dropdown-item>
-        <b-dropdown-item href="#">Signout</b-dropdown-item>
-      </b-dropdown>
+        <img src="../../assets/agzou.jpg" width="32px" height="32px">
+        <b-nav-item-dropdown right variant="default" size="sm" no-caret="true">
+          <template slot="button-content">
+            {{UserAccount}}
+          </template>
+          <b-dropdown-item href="#" to="/profile">用户中心</b-dropdown-item>
+          <b-dropdown-item href="#">常用快捷操作</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item href="#">意见反馈</b-dropdown-item>
+          <b-dropdown-item href="#">版本更新</b-dropdown-item>
+          <b-dropdown-item href="#">返回首页</b-dropdown-item>
+          <b-dropdown-divider></b-dropdown-divider>
+          <b-dropdown-item href="#">退出 </b-dropdown-item>
+        </b-nav-item-dropdown>
       </b-button-group>
     </b-nav>
   </b-navbar>
@@ -44,11 +64,19 @@
 
 
 <script>
+  import MenuBoard from '../../common/header/menu-board.vue'
 export default {
+  components:{
+      MenuBoard
+  },
   data() {
     return {
-      isSticky: true
+      isSticky: true,
+      UserAccount:''
     }
+  },
+  created(){
+    this.$ajax.get('/getUserInfo').then(res=>{this.UserAccount=res.data.UserAccount})
   }
 }
 
@@ -62,7 +90,7 @@ export default {
 #header-search {
   border-radius: 9px;
   background-color: rgba(255, 255, 255, 0.3);
-  border: none; 
+  border: none;
 }
 
 .header-center {
@@ -81,11 +109,43 @@ export default {
 
 .header-center li a {
   color: white;
+  cursor: pointer;
 }
 
 .ml-auto button {
   height: 32px;
   background-color: rgba(255, 255, 255, 0.3);
-  color:rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
+  cursor: pointer;
 }
+.ml-auto .nav-link:link {
+  font-size: 14px;
+  line-height: 14px;
+  height: 32px;
+  background-color: rgba(255, 255, 255, 0.3);
+  color: rgb(255, 255, 255);
+  border-radius: 4px;
+}
+.ml-auto .nav-link {
+  font-size: 14px;
+  line-height: 14px;
+  height: 32px;
+  background-color: rgba(255, 255, 255, 0.3);
+  color: rgb(255, 255, 255);
+  border-radius: 4px;
+}
+  .newButton{
+    width: 300px;
+  }
+  .newButton p{
+    font-size: 12px;
+    color: rgb(140,140,140);
+    cursor: pointer;
+  }
+  .newButton span{
+    font-size: 14px;
+    color: black;
+    cursor: pointer;
+  }
+
 </style>
---
+++ src/common/header/menu-board.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="menu-board">
+<div style="border-bottom: 1px rgb(140,140,140) inset;background-color:#F5F5F5 ">看板导航</div>
+<b-form-input  placeholder="输入看板名称进行过滤..." size="sm"></b-form-input>
+  <b-btn  v-b-toggle.c1 variant="default"><icon name="user" scale="1"></icon><span>个人看板</span></b-btn>
+  <b-collapse id="c1">
+    <b-card>
+      Content
+    </b-card>
+  </b-collapse>
+</div>
+</template>
+
+<script >
+export default{
+    data(){
+        return{
+
+        }
+    }
+}
+
+</script>
+
+<style>
+.menu-board{
+  width: 300px;
+  height: 500px;
+  overflow: auto;
+}
+  .menu-board button {
+    width: 300px;
+    background-color: white;
+    color: #428BCA;
+    text-align: left;
+    font-size: 14px;
+    line-height: 14px;
+  }
+  .menu-board button span {
+    position: relative;
+    bottom: 3px;
+    line-height: 14px;
+    font-size: 14px;
+  }
+
+</style>
