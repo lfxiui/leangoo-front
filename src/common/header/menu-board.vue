b6ec1fe82367fecbec4d4635b9b2e084f1b13aed
agzou <agzou@foxmail.com>
2017-09-06 10:20:35 +0800

    看板导航搜索完成:模糊搜索不准确问题待解决 by agzou 2017-9-6 10:20:32

--- src/common/header/menu-board.vue
+++ src/common/header/menu-board.vue
@@ -1,47 +1,130 @@
 <template>
-<div class="menu-board">
-<div style="border-bottom: 1px rgb(140,140,140) inset;background-color:#F5F5F5 ">
-  看板导航</div>
-<b-form-input  placeholder="输入看板名称进行过滤..." size="sm"></b-form-input>
-  <b-btn  v-b-toggle.c1 variant="default"><icon name="user" scale="1"></icon><span>个人看板</span></b-btn>
-  <b-collapse id="c1">
-    <b-card>
-      Content
-    </b-card>
-  </b-collapse>
-</div>
+  <div class="menu-board">
+    <div style="border-bottom: 1px rgb(140,140,140) inset;background-color:#F5F5F5 ">
+      看板导航</div>
+    <b-form-input placeholder="输入看板名称进行过滤..." size="sm" v-model="term"></b-form-input>
+    <div class="nav-board">
+      <b-btn v-b-toggle.c1 variant="default">
+        <icon name="user" scale="1"></icon>
+        <span>个人看板</span>
+      </b-btn>
+      <b-collapse id="c1">
+        <div>
+          <b-list-group style="margin:7px">
+            <b-list-group-item v-for="(item,index) in personalResult" :key="index" :tag="'a'" style="margin-bottom:4px;height:40px;border-radius:4px;">
+              <span class="thumbnail"></span>
+              <span class="board-list-item-name">{{item.BoardName}}</span>
+            </b-list-group-item>
+          </b-list-group>
+        </div>
+      </b-collapse>
+    </div>
+    <div class="nav-board" v-for="(Project,index) in  searchResult" :key="index">
+      <b-btn v-b-toggle="'m-collapse'+index" variant="default">
+        <icon name="cubes" scale="1"></icon>
+        <span>{{Project.ProjectName}}</span>
+      </b-btn>
+      <b-collapse :id="'m-collapse'+index">
+        <div>
+          <b-list-group style="margin:7px">
+            <b-list-group-item v-for="(item,index) in Project.BoardList" :key="index" :tag="'a'" style="margin-bottom:4px;height:40px;border-radius:4px;">
+              <span class="thumbnail"></span>
+              <span class="board-list-item-name">{{item.BoardName}}</span>
+            </b-list-group-item>
+          </b-list-group>
+        </div>
+      </b-collapse>
+    </div>
+  </div>
 </template>
 
 <script >
-export default{
-    data(){
-        return{
-
-        }
+import Bus from '../../bus.js'
+export default {
+  data() {
+    return {
+      term: '',
+      PersonalBoard: [],
+      ProjectList: [],
+      searchResult: [],
+      personalResult: [],
     }
+  },
+  watch: {
+    term() {
+      if (this.term != '') {
+        this.$search(this.term, this.ProjectList, { keys: ["BoardList.BoardName"] }).then(result => {
+          this.searchResult = result
+        });
+        this.$search(this.term, this.PersonalBoard, { keys: ["BoardName"] }).then(result => {
+          this.personalResult = result
+        });
+      }
+      else {
+        this.searchResult = this.ProjectList;
+        this.personalResult = this.PersonalBoard
+      }
+    }
+  },
+  created() {
+    Bus.$on('initPersonalBoard', PersonalBoard => {
+      this.PersonalBoard = PersonalBoard;
+      this.personalResult=PersonalBoard;
+    });
+    Bus.$on('initProjectList', ProjectList => {
+      this.ProjectList = ProjectList;
+      this.searchResult=ProjectList;
+    })
+  }
 }
 
 </script>
 
 <style>
-.menu-board{
+.menu-board {
   width: 300px;
   height: 500px;
   overflow: auto;
 }
-  .menu-board button {
-    width: 300px;
-    background-color: white;
-    color: #428BCA;
-    text-align: left;
-    font-size: 14px;
-    line-height: 14px;
-  }
-  .menu-board button span {
-    position: relative;
-    bottom: 3px;
-    line-height: 14px;
-    font-size: 14px;
-  }
 
+.menu-board button {
+  width: 300px;
+  background-color: white;
+  color: #428BCA;
+  text-align: left;
+  font-size: 14px;
+  line-height: 14px;
+}
+
+.menu-board button span {
+  position: relative;
+  bottom: 3px;
+  line-height: 14px;
+  font-size: 14px;
+}
+
+.thumbnail {
+  position: absolute;
+  background-color: #0E74AF;
+  top: 0;
+  left: 0;
+  width: 45px;
+  height: 100%;
+  opacity: .7;
+  display: block;
+  border-top-left-radius: 4px;
+  border-bottom-left-radius: 4px;
+}
+
+.board-list-item-name {
+  overflow: hidden;
+  margin-left: 26px;
+  padding-left: 10px;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.nav-board {
+  margin-top: 1px;
+}
 </style>
